799537985ad47b39158bb2da8e64beac7f9a78a3
ouailb <adomeyou@gmail.com>
2024-02-13 12:55:30 +0100

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {"User-Agent": "Custom User Agent"}  
 
     try:
         response = requests.get(url, headers=headers)
@@ -22,15 +22,4 @@ def number_of_subscribers(subreddit):
     except Exception as e:
         print(f"An error occurred: {e}")
         return 0
-'''
-if __name__ == "__main__":
-    import sys
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print(f"{subscribers}")
-
-'''
