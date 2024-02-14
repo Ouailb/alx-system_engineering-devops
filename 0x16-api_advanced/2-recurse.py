525ed8470c2c1542b2932188fe3a37a123798d42
ouailb <adomeyou@gmail.com>
2024-02-14 19:55:49 +0100

    t2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,44 +5,46 @@ all hot articles for a given subreddit.  """
 import requests
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """
-    Recursively retrieve the titles of all hot articles for a given subreddit.
+api_url = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
-    Parameters:
-    - subreddit (str): The name of the subreddit.
-    - hot_list (list): List to accumulate the titles of hot
-    articles (default is None).
-    - after (str): Token indicating the last post on the current
-    page (default is None).
 
-    Returns:
-    - List of titles if posts are found, None otherwise.
-    """
-    if hot_list is None:
-        hot_list = []
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': '0x16-api_advanced:/1.0.0 (by /u/aboubakrtaibi)'}
-    params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            posts = data['data']['children']
-            after = data['data']['after'] if 'after' in data['data'] else None
-            hot_list.extend(post['data']['title'] for post in posts)
-            if after:
-                recurse(subreddit, hot_list, after=after)
+def recurse(subreddit, hot_list=[], n=0, after=None):
+    '''Retrieves a list of hot posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'hot'
+    limit = 30
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
+            api_url,
+            subreddit,
+            sort,
+            limit,
+            n,
+            after if after else ''
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        data = res.json()['data']
+        posts = data['children']
+        count = len(posts)
+        hot_list.extend(list(map(lambda x: x['data']['title'], posts)))
+        if count >= limit and data['after']:
+            return recurse(subreddit, hot_list, n + count, data['after'])
         else:
-            print("Unexpected response format from Reddit API.")
-
-    elif response.status_code == 302:
-        print(f"Invalid subreddit: {subreddit}")
-        return None
-
+            return hot_list if hot_list else None
     else:
-        print(f"Failed. Status code: {response.status_code}")
-
-    return hot_list
-
+        return hot_list if hot_list else None
